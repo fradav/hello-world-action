fc01c5385e826490497b772b7d750cd9e64e3db4
François-David Collin <Francois-David.Collin@umontpellier.fr>
2020-10-27 04:04:53 +0100

    truc

--- setup.py
+++ setup.py
@@ -1,21 +1,46 @@
-from setuptools import setup, find_packages
+from setuptools import setup
+
 from os import getenv
 
 print("CMAKEARGSFILE : ",getenv("CMAKEARGSFILE"))
 
+# Available at setup time due to pyproject.toml
+from pybind11.setup_helpers import Pybind11Extension, build_ext
+from pybind11 import get_cmake_dir
+
+import sys
+
+__version__ = "0.0.1"
+
+# The main interface is through Pybind11Extension.
+# * You can add cxx_std=11/14/17, and then build_ext can be removed.
+# * You can set include_pybind11=false to add the include directory yourself,
+#   say from a submodule.
+#
+# Note:
+#   Sort input source files if you glob sources to ensure bit-for-bit
+#   reproducible builds (https://github.com/pybind/python_example/pull/53)
+
+ext_modules = [
+    Pybind11Extension("python_example",
+        ["src/main.cpp"],
+        # Example: passing in the version to the compiled code
+        define_macros = [('VERSION_INFO', __version__)],
+        ),
+]
+
 setup(
-    name="hello_world_python",
-    version="0.0.4",
-    description="Test github actions",
-    url="https://github.com/garimaarorashuttl/hello-world-python.git",
-    author="garimaarora",
-    author_email="garimaarora786@gmail.com",
-    license="MIT",
-    packages=find_packages(),
-    classifiers=["Programming Language :: Python :: 3.7"],
-    install_requires=["shapely", "pytz"],
-    extras_require={
-        "test": ["pytest", "pytest-runner", "pytest-cov", "pytest-pep8"],
-        "dev": ["flake8"],
-    },
+    name="python_example",
+    version=__version__,
+    author="Sylvain Corlay",
+    author_email="sylvain.corlay@gmail.com",
+    url="https://github.com/pybind/python_example",
+    description="A test project using pybind11",
+    long_description="",
+    ext_modules=ext_modules,
+    extras_require={"test": "pytest"},
+    # Currently, build_ext only provides an optional "highest supported C++
+    # level" feature, but in the future it may provide more features.
+    cmdclass={"build_ext": build_ext},
+    zip_safe=False,
 )
