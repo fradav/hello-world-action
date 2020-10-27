677334d108444d82be2a83143ceea214c5b8237c
François-David Collin <Francois-David.Collin@umontpellier.fr>
2020-10-27 03:51:32 +0100

    first init

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+from os import getenv
+
+print("CMAKEARGSFILE : ",getenv("CMAKEARGSFILE"))
+
+setup(
+    name="hello_world_python",
+    version="0.0.4",
+    description="Test github actions",
+    url="https://github.com/garimaarorashuttl/hello-world-python.git",
+    author="garimaarora",
+    author_email="garimaarora786@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    classifiers=["Programming Language :: Python :: 3.7"],
+    install_requires=["shapely", "pytz"],
+    extras_require={
+        "test": ["pytest", "pytest-runner", "pytest-cov", "pytest-pep8"],
+        "dev": ["flake8"],
+    },
+)
